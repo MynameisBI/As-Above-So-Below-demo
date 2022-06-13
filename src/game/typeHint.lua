77f79729e27a5969b4bc0349916dd28efc681256
MynameisBI <56908557+MynameisBI@users.noreply.github.com>
2022-06-13 20:38:48 +0700

    fix tHint bug

--- src/game/typeHint.lua
+++ src/game/typeHint.lua
@@ -21,8 +21,10 @@ function TypeHint:onCardChanged()
 end
 
 function TypeHint:getHint(leftCard, rightCard)
-	if leftCard.group == rightCard.group then return '='
-	elseif leftCard.type == rightCard.type then return '~'
+	if leftCard.group == rightCard.group then
+		if leftCard.type == rightCard.type then return '='
+		else return '~'
+		end
 	else return '=/='
 	end
 end
